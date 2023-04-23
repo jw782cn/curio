a2ff1f9ae5f56f778d51870d23fe68dd89a283a7
Ning <cn257@cornell.edu>
2023-04-23 16:14:10 -0400

    update graph

--- backend/graph.py
+++ backend/graph.py
@@ -1,12 +1,37 @@
-
+import json
+import chatgpt
 
 class graph():
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, topic):
+        self.topic = topic
         self.graph = self.create_graph()
+        self.chatgpt = chatgpt()
         
-    def create_graph(topic : str):
-        pass
+        
+    def create_graph(self):
+        # demo
+        # open file "sample.json"
+        with open("sample.json", "r") as f:
+            graph = json.load(f)
+        return graph
+        
+    def init_graph(self):
+        # demo
+        # open file "sample.json"
+        graph = { "nodes" : [
+            {
+            "id": "0",
+            "name": self.topic,
+            "symbolSize": 25,
+            "x": -266.82776,
+            "y": 299.6904,
+            "value": 28.685715,
+            "category": 0
+        },
+            ], "links" : [], "categories" : [] }
+        
+        return graph
+        
         
     def update_graph():
         pass
