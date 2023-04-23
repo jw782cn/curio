edfbbf9757adf2087118cc358ca38475bf390e38
Ning <cn257@cornell.edu>
2023-04-23 15:59:08 -0400

    update

---
+++ backend/graph.py
@@ -0,0 +1,16 @@
+
+
+class graph():
+    def __init__(self, data):
+        self.data = data
+        self.graph = self.create_graph()
+        
+        
+    def create_graph(topic : str):
+        pass
+        
+    def update_graph():
+        pass
+    
+    def update_data():
+        pass
